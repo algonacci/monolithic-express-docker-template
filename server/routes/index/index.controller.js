3f3a5e11f411f5bc5e2418811bb9e1235228d6bc
fajrCode <fajri42vandi@gmail.com>
2024-03-16 22:56:12 +0700

    update template

--- server/routes/index/index.controller.js
+++ server/routes/index/index.controller.js
@@ -1,5 +1,5 @@
 const getIndexPage = (req, res) => {
-  res.render("pages/index", { title: "Hi Kids", message: "Welcome to Monolithic Express Templating!" });
+  res.render("pages/index/index", { title: "Hi Kids", message: "Welcome to Monolithic Express Templating!" });
 };
 
 module.exports = { getIndexPage };
